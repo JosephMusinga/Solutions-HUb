1a8ee30a5a9d1c38971f62082e52358cd40e463a
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-02-28 16:56:43 +0200

    1.00.00

--- solhub/base/urls.py
+++ solhub/base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="HOME"),
-    path('room/', views.room, name="ROOM"),
+    path('room/<str:pk>', views.room, name="ROOM"),
 ]
 
 
--- solhub/base/views.py
+++ solhub/base/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+rooms=[
+    {'id':1, 'name':'Dept1'},
+    {'id':2, 'name':'Dept2'},
+    {'id':3, 'name':'Dept3'},
 
-def room(request):
-    return render(request, 'room.html')
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
