71c7a2ef5af8c42ac1e7c69df284ccba20e0a35d
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-03-20 09:55:16 +0200

    3.53.35

--- solhub/base/views.py
+++ solhub/base/views.py
@@ -9,13 +9,6 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
 from .forms import RoomForm
 
-# Create your views here.
-# rooms=[
-#     {'id':1, 'name':'Dept1'},
-#     {'id':2, 'name':'Dept2'},
-#     {'id':3, 'name':'Dept3'},
-
-# ]
 
 def loginPage(request):
     page = 'login'
@@ -96,6 +89,15 @@ def room(request, pk):
 
     context = {'room':room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    return render(request, 'base/profile.html', context)
+
 
 @login_required(login_url='login')
 def createRoom(request):
