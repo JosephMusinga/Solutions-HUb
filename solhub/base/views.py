eca32a8160c450848799c95379bdafcf210b13bb
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-03-20 16:29:23 +0200

    4.26.00

--- solhub/base/views.py
+++ solhub/base/views.py
@@ -106,6 +106,8 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
             form.save()
             return redirect('home')
 
