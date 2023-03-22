086573da8bd06f3f1644a7309c39b3b45073b3fe
Jos <98326351+JosephMusinga@users.noreply.github.com>
2023-03-22 15:39:37 +0200

    5.O4.00

--- solhub/base/views.py
+++ solhub/base/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
 from .forms import RoomForm
@@ -13,7 +13,7 @@ from .forms import RoomForm
 def loginPage(request):
     page = 'login'
 
-    #avoiding relogging in of an already logged in user
+    # avoiding relogging in of an already logged in user
     if request.user.is_authenticated:
         return redirect('home')
 
@@ -29,7 +29,7 @@ def loginPage(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            login(request, user )
+            login(request, user)
             return redirect('home')
         else:
             messages.error(request, 'Incorrect username or password')
@@ -42,9 +42,10 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
 def registerPage(request):
     form = UserCreationForm()
-    
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -58,6 +59,7 @@ def registerPage(request):
 
     return render(request, 'base/login_register.html', {'form': form})
 
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
@@ -65,17 +67,19 @@ def home(request):
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
-        )
+    )
 
     topics = Topic.objects.all()
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
-    context = {'rooms':rooms, 'topics':topics, 'room_count':room_count, 'room_messages':room_messages}
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
+
 def room(request, pk):
-    room = Room.objects.get(id = pk)
+    room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
     participants = room.participants.all()
     if request.method == 'POST':
@@ -87,22 +91,25 @@ def room(request, pk):
         room.participants.add(request.user)
         return redirect('room', pk=room.id)
 
-    context = {'room':room, 'room_messages':room_messages, 'participants':participants}
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
+
 
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
     room_messages = user.message_set.all()
     topics = Topic.objects.all()
-    context = {'user':user, 'rooms':rooms, 'room_messages':room_messages, 'topics':topics}
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
 
 
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm
-    
+    topics = Topic.objects.all()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -111,13 +118,15 @@ def createRoom(request):
             form.save()
             return redirect('home')
 
-    context = {'form' : form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
+
 @login_required(login_url='login')
-def updateRoom(request, pk ):
+def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not the owner of the room !')
@@ -128,32 +137,33 @@ def updateRoom(request, pk ):
             form.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
+
 
 @login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
-    
+
     if request.user != room.host:
         return HttpResponse('You are not the owner of the room !')
 
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    
-    
-    return render (request, 'base/delete.html', {'obj':room})
+
+    return render(request, 'base/delete.html', {'obj': room})
+
 
 @login_required(login_url='login')
 def deleteMessage(request, pk):
     message = Message.objects.get(id=pk)
-    
+
     if request.user != message.user:
         return HttpResponse('You are not the owner of the room !')
 
     if request.method == 'POST':
         message.delete()
         return redirect('home')
-    
-    return render (request, 'base/delete.html', {'obj':message})
+
+    return render(request, 'base/delete.html', {'obj': message})
